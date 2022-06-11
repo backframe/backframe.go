4f0e2349ad4f527660f05059d6ff7698b92f981a
Victor Ndaba <mwangindaba@gmail.com>
2022-06-11 03:26:24 +0300

    📦 feat: got the serialize functionality working

--- bf-core/parser/serialize.go
+++ bf-core/parser/serialize.go
@@ -1,28 +1,50 @@
 package parser
 
-var interfaceTmpl = `
-interface {{name}} {
-	isVersioned = {{Versioned}}
+import (
+	"fmt"
+	"strings"
+)
 
-	if isVersioned {
-		${{versions}}
-	} else {
-		${{resources}}
+func Serialize(blocks []Block) []byte {
+	var contents strings.Builder
+	// Start with each section
+	for _, b := range blocks {
+		// b is a section
+		contents.WriteString(WriteBlock(b))
+		contents.WriteString("\n")
 	}
+	return []byte(contents.String())
 }
-`
 
-var resourceTmpl = `
-resource {{type}} {
-	schema = {{schema}}
+func WriteBlock(b Block) string {
+	var tmpl strings.Builder
+	var blocksTmpl strings.Builder
 
-	${{methods}}
+	tmpl.WriteString(fmt.Sprintf("%v %v { \n", strings.ToLower(b._type), b.id))
+
+	// handle block variables
+	for k, v := range b.variables {
+		if strings.ContainsAny(v, "|") {
+			// its an array
+			val := strings.ReplaceAll(v, "|", ",")
+			tmp := fmt.Sprintf("%v = [%v,];", k, val)
+			tmpl.WriteString(fmt.Sprintf("\t%v\n", tmp))
+		} else {
+			tmp := fmt.Sprintf("%v = %v;", k, v)
+			tmpl.WriteString(fmt.Sprintf("\t%v\n", tmp))
+		}
+	}
+
+	// handle nested blocks
+	for i := 0; i < len(b.blocks); i++ {
+		currentBlock := b.blocks[i]
+		tmpl := WriteBlock(currentBlock)
+		blocksTmpl.WriteString(tmpl)
+	}
+	if len(blocksTmpl.String()) > 0 {
+		tmpl.WriteString(fmt.Sprintf("%v\n", blocksTmpl.String()))
+	}
+	tmpl.WriteString("}")
+
+	return tmpl.String()
 }
-`
-var methodTmpl = `
-method {{name}} {
-	secured = {{secured}}
-	pubfields = {{pubfields}}
-	subresources = {{subresources}}
-}
-`
