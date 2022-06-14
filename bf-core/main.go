ccd6480cef3e582bd10c0256c3663dd7d433e116
Victor Ndaba <mwangindaba@gmail.com>
2022-06-14 21:57:24 +0300

    🧹 chore(bf-core): updated main.go test

--- bf-core/main.go
+++ bf-core/main.go
@@ -2,13 +2,60 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
+	"os"
 
-	"backframe.io/backframe/bf-core/parser"
+	"backframe.io/backframe/bf-core/internal/parser"
+	"backframe.io/backframe/bf-core/internal/serde"
 )
 
 func main() {
 	p := parser.Parser{}
-	ast := p.Parse(`section`)
+	ast := p.Parse(`  
+	
+	# the providers section
+	section Interfaces {
+		interface Rest {
+			isVersioned = true;
+			
+			version 1 {
+				resource User {
+					schema = "models/user.schema";
+	
+					method GET {
+						# secured = true;
+					
+						pubfields = [
+						"name", 
+						"position",
+						];
+					}
+				}
+				
+				resource Book {
+					schema = "models/book.schema";
+	
+					method POST {}
+				}
+			}
 
-	fmt.Printf("%v", ast)
+
+		}
+
+		interface Graphql {}
+	}
+
+	section Providers {
+		provider Google {}
+
+		provider Facebook {}
+	}
+
+	
+	`)
+
+	fmt.Printf("%v\n", ast)
+	contents := serde.Serialize(ast)
+
+	os.WriteFile("Specfile", contents, fs.FileMode(os.O_RDWR))
 }
