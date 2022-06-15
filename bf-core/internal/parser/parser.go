6e288c080eafdeb40cbe2a2b3829ade92f2cdc8b
Victor Ndaba <mwangindaba@gmail.com>
2022-06-15 22:54:40 +0300

    🔃 refactor(bf-core): rewrote parser workflow and added error reporting

--- bf-core/internal/parser/parser.go
+++ bf-core/internal/parser/parser.go
@@ -6,23 +6,39 @@ import (
 )
 
 type Parser struct {
-	_string    string
-	_lexer     Lexer
-	_lookahead Token
+	_string     string
+	_lexer      Lexer
+	_lookahead  Token
+	knownBlocks []string
 }
 
 func NewParser() Parser {
 	return Parser{
 		_string: "",
 		_lexer:  Lexer{},
+		knownBlocks: []string{
+			"INTERFACE",
+			"PROVIDER",
+			"INTEGRATION",
+			"METHOD",
+			"VERSION",
+			"MIGRATION",
+			"DATABASE",
+			"RESOURCE",
+		},
 	}
 }
 
 type Block struct {
-	Type      string
-	Id        string
-	Variables map[string]string
-	Blocks    []Block
+	Type  string
+	Id    string
+	Body  []Block
+	Value string
+}
+
+type SpecAst struct {
+	Type     string
+	Sections []Block
 }
 
 /*
@@ -59,63 +75,81 @@ Literal
 	;
 
 */
-func (p *Parser) Parse(val string) []Block {
+func (p *Parser) Parse(val string) SpecAst {
 	p._string = val
 	p._lexer.init(val)
 	p._lookahead, _ = p._lexer.getNextToken()
 
-	return p.blockStatement()
+	return SpecAst{
+		Type:     "Specfile",
+		Sections: p.sectionList(),
+	}
 }
 
-func (p *Parser) statementList() (map[string]string, []Block) {
-	variables, blocks := p.statement()
+func (p *Parser) sectionList() []Block {
+	blocks := []Block{p.section()}
+
+	for {
+		if !p._lexer.hasMoreTokens() {
+			break
+		}
+
+		blocks = append(blocks, p.section())
+	}
+
+	return blocks
+}
+
+func (p *Parser) section() Block {
+	p._eat("SECTION")
+	id := p._eat("IDENTIFIER")
+	p._eat("{")
+	body := p.statementList()
+	p._eat("}")
+
+	return Block{
+		Type: "SECTION",
+		Id:   id.value,
+		Body: body,
+	}
+
+}
+
+func (p *Parser) statementList() []Block {
+	stmnts := []Block{}
 
 	for {
 		if p._lookahead._type == "}" {
 			break
 		}
-		newVars, newBlocks := p.statement()
-		blocks = append(blocks, newBlocks...)
-		for k, v := range newVars {
-			variables[k] = v
-		}
+		stmnts = append(stmnts, p.statement())
 	}
 
-	return variables, blocks
+	return stmnts
 }
 
-func (p *Parser) statement() (map[string]string, []Block) {
-	switch p._lookahead._type {
-	case "IDENTIFIER":
-		return p.expressionStatement(), []Block{}
-	default:
-		return make(map[string]string), p.blockStatement()
+func (p *Parser) statement() Block {
+	value := p._lexer._string
+	idx := p._lexer._cursor + 1
+
+	if string(value[idx]) == "=" {
+		return p.expressionStatement()
+	} else {
+		return p.blockStatement()
 	}
 }
 
-func (p *Parser) expressionStatement() map[string]string {
-	values := make(map[string]string)
+func (p *Parser) expressionStatement() Block {
+	id := p._eat("IDENTIFIER")
+	p._eat("ASSIGNMENT_OP")
+	token := p.literal()
 
-	for {
-		if p._lookahead._type == "}" || p._lookahead._type != "IDENTIFIER" {
-			break
-		}
-
-		for {
-			if p._lookahead._type == "}" || p._lookahead._type == ";" || p._lookahead._type != "IDENTIFIER" {
-				break
-			}
-			v := p._eat("IDENTIFIER")
-			p._eat("ASSIGNMENT_OP")
-			// TODO: Implement literals
-			id := p.literal()
-
-			values[v.value] = id.value
-			p._eat(";")
-		}
+	p._eat(";")
+	return Block{
+		Type:  "ASSIGNMENT",
+		Id:    id.value,
+		Value: token.value,
 	}
-
-	return values
 }
 
 func (p *Parser) literal() Token {
@@ -152,40 +186,46 @@ func (p *Parser) array() Token {
 	}
 }
 
-func (p *Parser) blockStatement() []Block {
-	list := []Block{}
+func (p *Parser) blockStatement() Block {
+	name := p.blockType()
+	id := p._eat("IDENTIFIER")
+	p._eat("{")
 
-	for {
-		if p._lookahead._type == "}" || !p._lexer.hasMoreTokens() {
-			break
+	body := p.statementList()
+	p._eat("}")
+	return Block{
+		Type: name._type,
+		Id:   id.value,
+		Body: body,
+	}
+}
+
+func (p *Parser) blockType() Token {
+	nxt := p._lookahead._type
+
+	for _, b := range p.knownBlocks {
+		if b == nxt {
+			return p._eat(b)
 		}
-		v := p._eat(p._lookahead._type)
-		id := p._eat("IDENTIFIER")
-		p._eat("{")
-
-		b := Block{
-			Type: v._type,
-			Id:   id.value,
-		}
-
-		vars, blocks := p.statementList()
-		b.Variables = vars
-		b.Blocks = blocks
-
-		list = append(list, b)
-
-		p._eat("}")
 	}
 
-	return list
+	panic(fmt.Sprintf("SyntaxError on line: %d, column: %d. Unknown block: `%s` found", p.getLine(), p.getCol(), nxt))
 }
 
 func (p *Parser) _eat(_type string) Token {
 	token := p._lookahead
 	if _type != token._type {
-		panic(fmt.Sprintf("Syntax Error, Unexpected Token. Expected: %v but instead found: %v\n", _type, token._type))
+		panic(fmt.Sprintf("SyntaxError on line: %d, column: %d. Unexpected Token. Expected: %v but instead found: %v\n", p.getLine(), p.getCol(), _type, token._type))
 	}
 
 	p._lookahead, _ = p._lexer.getNextToken()
 	return token
+}
+
+func (p *Parser) getLine() int {
+	return p._lexer.line
+}
+
+func (p *Parser) getCol() int {
+	return p._lexer.column
 }
